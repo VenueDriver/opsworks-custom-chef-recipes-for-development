547f7789841d876b655e137db611872655376a66
Ryan Alyn Porter <rap@endymion.com>
2016-01-02 20:23:56 -0500

    auto yes

--- recipes/default.rb
+++ recipes/default.rb
@@ -50,6 +50,6 @@ end
 execute "Install Virtualbox" do
   user "ubuntu"
   command <<EOF
-sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list" && wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - && sudo apt-get update && sudo apt-get install virtualbox-5.0
+sudo sh -c "echo 'deb http://download.virtualbox.org/virtualbox/debian '$(lsb_release -cs)' contrib non-free' > /etc/apt/sources.list.d/virtualbox.list" && wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add - && sudo apt-get update && sudo apt-get -y install virtualbox-5.0
 EOF
 end
