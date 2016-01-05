ffbf1befe4cce4e3f16e414972fa48e955098c80
Ryan Alyn Porter <rap@endymion.com>
2016-01-05 16:13:47 -0500

    Install Docker Compose.

--- recipes/default.rb
+++ recipes/default.rb
@@ -31,3 +31,10 @@ end
 package "Install Docker engine" do
   package_name %w(docker-engine)
 end
+
+execute "Install Docker Compose" do
+  command <<-EOC
+    curl -L https://github.com/docker/compose/releases/download/1.5.2/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+  EOC
+end
